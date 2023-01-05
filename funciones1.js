9e7965c23bd14cbd16b1e3b0d0689ffd243c87d9
Marcos Rios <max.rios92@gmail.com>
2023-01-05 19:32:30 -0300

    nuevas pruebas

---
+++ funciones1.js
@@ -0,0 +1,9 @@
+
+document.getElementById('temaclaro').onclick = function claro() {
+    document.body.style.backgroundColor = 'white';
+}
+
+document.getElementById('temaoscuro').onclick = function oscuro() {
+    document.body.style.backgroundColor = 'black';
+}
+
