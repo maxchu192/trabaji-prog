e00e477d41c73147a4403ac71e6d50964fed06c9
Marcos Rios <max.rios92@gmail.com>
2023-01-04 22:16:09 -0300

    modificaciones estructura/estilos y agregado de funciones

---
+++ proyecto-web/pruebas/funciones/funcionesdelproyecto.js
@@ -0,0 +1,12 @@
+
+
+document.getElementById('temaclaro').onclick = function claro() {
+    document.body.style.backgroundColor = '#add8e6'
+}
+
+document.getElementById('temaoscuro').onclick = function oscuro() {
+    document.body.style.backgroundColor = '#000000'
+}
+
+
+
