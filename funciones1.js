a342ec8814d0068e2a8cd002de75675e29e9d06b
Marcos Rios <max.rios92@gmail.com>
2023-01-06 00:10:15 -0300

    modificaion de nombre a archivos y detalles

--- funciones1.js
+++ funciones1.js
@@ -1,9 +1,14 @@
 
 document.getElementById('temaclaro').onclick = function claro() {
-    document.body.style.backgroundColor = 'white';
+    document.body.style.backgroundColor = 'lightgreen';
+    document.body.style.color = 'black';
 }
 
 document.getElementById('temaoscuro').onclick = function oscuro() {
-    document.body.style.backgroundColor = 'black';
+    document.body.style.backgroundColor = 'darkgreen';
+    document.body.style.color = 'lightcyan';
 }
 
+document.getElementById('boton').onclick = function cambiar() {
+    document.getElementById('respuesta').style.display='block';
+}
