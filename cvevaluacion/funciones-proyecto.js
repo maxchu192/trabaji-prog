35fd6ed1028c705062b923416c4ed75d9e06a4fa
Marcos Rios <max.rios92@gmail.com>
2023-01-08 02:57:59 -0300

    archivo para evaluacion

---
+++ cvevaluacion/funciones-proyecto.js
@@ -0,0 +1,36 @@
+
+document.getElementById('temaclaro').onclick = function claro() {
+    document.body.style.backgroundColor = 'white';
+    document.body.style.color = 'black';
+}
+
+document.getElementById('temaoscuro').onclick = function oscuro() {
+    document.body.style.backgroundColor = 'black';
+    document.body.style.color = 'white';
+}
+
+document.getElementById('boton').onclick = function cambiar() {
+    document.getElementById('respuesta').style.display='block';
+}
+
+/*intento de agregar menu desplegable y carrousel fallidos*/
+
+
+/*$(document).ready(main);
+var contador = 0;
+function main(){
+    $('.menu').click(function(){
+        contador = (contador + 1)%2
+        if(contador == 1){
+            $('nav').animate({
+                left: '0'
+            });
+            
+        } else {
+            
+            $('nav').animate({
+                left: '-100%'
+            });
+        }
+    });
+};*/
