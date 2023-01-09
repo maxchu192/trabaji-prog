d2feced34208adacfd3dc101ae5f6b05fcd4bd38
Marcos Rios <max.rios92@gmail.com>
2023-01-09 20:51:43 -0300

    agregado de footer y funciones

--- funciones2.js
+++ funciones2.js
@@ -16,3 +16,18 @@ function main(){
         }
     });
 };
+
+
+document.getElementById('temaclaro').onclick = function claro() {
+    document.body.style.backgroundColor = 'lightgreen';
+    document.body.style.color = 'black';
+}
+
+document.getElementById('temaoscuro').onclick = function oscuro() {
+    document.body.style.backgroundColor = 'darkgreen';
+    document.body.style.color = 'white';
+}
+
+document.getElementById('boton').onclick = function cambiar() {
+    document.getElementById('respuesta').style.display='block';
+}
